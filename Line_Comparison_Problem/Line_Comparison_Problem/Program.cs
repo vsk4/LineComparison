d9514c9caad0badda6c85e9f874fd25a67019b8b
vsk4 <saikumarvadla4@gmail.com>
2020-10-03 10:24:01 +0530

    [VSK]:Refrctor added UC3-LineComparison

--- Line_Comparison_Problem/Line_Comparison_Problem/Program.cs
+++ Line_Comparison_Problem/Line_Comparison_Problem/Program.cs
@@ -1,13 +1,59 @@
 ﻿using System;
 
-namespace Line_Comparison_Problem
+namespace LCP
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Console.WriteLine("Hello Welcome to LCP");
+            Console.WriteLine("Enter the coordiates");
 
+            double x1, y1, x2, y2, length_1;
+
+            Console.WriteLine("enter x1 ");
+            x1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("enter y1 ");
+            y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("enter x2 ");
+            x2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("enter y2 ");
+            y2 = Convert.ToDouble(Console.ReadLine());
+
+            double m1, m2, n1, n2, length_2;
+
+            Console.WriteLine("enter m1 ");
+            m1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("enter n1 ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("enter m2 ");
+            m2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("enter n2 ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+
+            length_1 = Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+            //Console.WriteLine("Legth of Line is:" + length1);
+
+            length_2 = Math.Sqrt(((m2 - m1) * (m2 - m1)) + ((n2 - n1) * (n2 - n1)));
+
+            if (length_1 == length_2)
+            {
+                Console.WriteLine("Lines are of equal length");
+            }
+            else if (length_1 > length_2)
+            {
+                Console.WriteLine("Line1 is bigger than Line2");
+            }
+            else
+            {
+                Console.WriteLine("Line2 is bigger than Line1");
+            }
+        }
     }
 }
